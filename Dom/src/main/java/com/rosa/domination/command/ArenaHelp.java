213755c78637fda26b2f67831f02183c3d8addd5
CenturyOrb <122176094+CenturyOrb@users.noreply.github.com>
2023-01-08 01:01:49 -0500

    Added /arena command
    Added /arena command, ignore changes to ArenaJoin class, imports were changed by Intellij automatically.

--- Dom/src/main/java/com/rosa/domination/command/ArenaHelp.java
+++ Dom/src/main/java/com/rosa/domination/command/ArenaHelp.java
@@ -1,13 +1,12 @@
 package com.rosa.domination.command;
 
-import org.bukkit.command.CommandSender;
-
-import com.rosa.domination.Domination;
-
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.CommandPermission;
 import co.aikar.commands.annotation.HelpCommand;
+import com.rosa.domination.Domination;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 @CommandAlias("arena")
 public class ArenaHelp extends BaseCommand {
@@ -20,6 +19,12 @@ public class ArenaHelp extends BaseCommand {
 	@HelpCommand
 	@CommandPermission("domination.arena.help")
 	public void help(final CommandSender sender) {
-		//todo if needed booger
+		if (!(sender instanceof Player))   { return; }
+		final Player player = (Player) sender;
+
+		player.sendMessage("#redAvailable arena commands:");
+		player.sendMessage("#yellow- /arena list (Shows list of arenas)");
+		player.sendMessage("#yellow- /arena leave (Leave your current arena)");
+		player.sendMessage("#yellow- /arena join <id> (Join an arena)");
 	}
 }
