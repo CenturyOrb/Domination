e453fb04ce98ebe5f0c901475948df6e45c25337
CenturyOrb <122176094+CenturyOrb@users.noreply.github.com>
2023-06-22 08:06:16 -0400

    Added Zones and Shops

--- Dom/src/main/java/com/rosa/domination/GameState.java
+++
@@ -1,8 +0,0 @@
-package com.rosa.domination;
-
-public enum GameState {
-
-    RECRUITING, // Player teleport from hub to lobby and wait for more plaers
-    BUY, // there are enough players, 2 minutes for players to create kit
-    LIVE; // game is live
-}
